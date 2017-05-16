c2e14964b071dd5694b4fea165d999cba2b2f2a7
ghersi.cherifa <ghersi.cherifa@gmail.com>
2017-05-16 10:39:16 +0200

    fix update slides

--- server/config/lib/express.js
+++ server/config/lib/express.js
@@ -87,7 +87,7 @@ module.exports.initMiddleware = function(app) {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
-  app.use(bodyParser.json());
+  app.use(bodyParser.json({ limit: '50mb' }));
   app.use(methodOverride());
 
   // Add the cookie parser and flash middleware
