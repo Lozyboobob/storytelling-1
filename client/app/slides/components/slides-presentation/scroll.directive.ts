c3fccb667340fc27f09cb1faa26b34127311b503
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-05-19 18:08:50 +0200

    fix perf events scroll

--- client/app/slides/components/slides-presentation/scroll.directive.ts
+++ client/app/slides/components/slides-presentation/scroll.directive.ts
@@ -1,47 +1,63 @@
-import { Directive, HostListener, Output, EventEmitter } from '@angular/core';
-
+import { Directive, HostListener, Output, EventEmitter, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
 @Directive({
     selector: '[appScroll]'
 })
-export class ScrollDirective {
+export class ScrollDirective implements OnInit {
     scrollPose: number = 1250; // scroll in same direction won't be triggered repeatedly inside <number> second
     private enableScroll: boolean = true;
+    private delayDuration: number = 200;
     @Output() mouseWheelUp = new EventEmitter();
     @Output() mouseWheelDown = new EventEmitter();
 
-    @HostListener('mousewheel', ['$event'])
-    onMouseWheelChrome(event: any) {
-        this.mouseWheelFunc(event);
-    }
-
-    @HostListener('DOMMouseScroll', ['$event']) onMouseWheelFirefox(event: any) {
-        this.mouseWheelFunc(event);
-    }
-
-    @HostListener('onmousewheel', ['$event']) onMouseWheelIE(event: any) {
-        this.mouseWheelFunc(event);
-    }
     constructor() {
-
     }
 
-    mouseWheelFunc(event: any) {
-      //  if (!this.enableScroll) return;
-           var event = window.event || event; // old IE support
-            // for IE
-            event.returnValue = false;
-            // for Chrome and Firefox
-            if (event.preventDefault) {
-                event.preventDefault();
-            }
+    ngOnInit(){
+        let events = Observable.fromEvent(window, 'mousewheel', (e) => e);
+        let firstEventObservable = events.take(1);
+        let remainingEventsObservable = events.skip(1).timeInterval()
+                .filter(x => x.interval >= this.delayDuration)
+                .map(x => x.value);
+        let pageChangeObservable = firstEventObservable.concat(remainingEventsObservable);
 
+        pageChangeObservable.subscribe(e => this.mouseWheelFunc(e));
+    }
+    // @HostListener('mousewheel', ['$event'])
+    // private onMouseWheelChrome(event: any) {
+    //     console.log('event => ', event);
+    //     Observable.of(event)
+    //         .do(console.log)
+    //         .debounceTime(30)
+    //         .map(this.mouseWheelFunc);
+    //     // this.mouseWheelFunc(event);
+    
+    // }
+
+    @HostListener('DOMMouseScroll', ['$event'])
+    private onMouseWheelFirefox(event: any) {
+        this.mouseWheelFunc(event);
+    }
+
+    @HostListener('onmousewheel', ['$event'])
+    private onMouseWheelIE(event: any) {
+        this.mouseWheelFunc(event);
+    }
+
+    private mouseWheelFunc(event: any) {
+        //  if (!this.enableScroll) return;
+        var event = window.event || event; // old IE support
+        // for IE
+        event.returnValue = false;
+        // for Chrome and Firefox
+        if (event.preventDefault) {
+            event.preventDefault();
+        }
         var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
         if (delta > 0) {
             this.mouseWheelUp.emit();
         } else if (delta < 0) {
             this.mouseWheelDown.emit();
         }
-
-
     }
 }
--- client/app/slides/components/slides-presentation/slides-presentation.component.ts
+++ client/app/slides/components/slides-presentation/slides-presentation.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit, Inject, ViewChild, ViewChildren, ElementRef, AfterViewInit, QueryList, AfterViewChecked, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Inject, ViewChildren, ViewEncapsulation } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { WindowResizeService } from '../../services/window-resize.service';
-//import { ScrollDirective} from './scroll.directive';
-import {PageScrollInstance, PageScrollService, PageScrollConfig} from 'ng2-page-scroll';
+import { PageScrollInstance, PageScrollService, PageScrollConfig } from 'ng2-page-scroll';
 import {DOCUMENT, DomSanitizer} from '@angular/platform-browser';
 import {SlidesService} from '../../services/slides.service';
 import { BarChartComponent, ForceDirectedGraphComponent, LineChartComponent} from 'app/charts';
 
 import { PageConfig, HALF_HALF_LAYOUT, FULL_LAYOUT} from './pageConfig';
+
 @Component({
     selector: 'app-slides-presentation',
     templateUrl: './slides-presentation.component.html',
     styleUrls: ['./slides-presentation.component.scss'],
-    providers: [WindowResizeService, SlidesService],
-    encapsulation: ViewEncapsulation.None
+    providers: [WindowResizeService, SlidesService]
 })
-export class SlidesPresentationComponent implements OnInit, AfterViewInit, AfterViewChecked {
+
+export class SlidesPresentationComponent implements OnInit {
     slides: Array<any> = [];
-    slideTextTransformed: Array<any> = [];
     slideTitle: String;
     slideHeight_style: any = {
         'height': '72px'
@@ -25,36 +24,36 @@ export class SlidesPresentationComponent implements OnInit, AfterViewInit, After
     slideHeight: number;
     curSlideIndex: number = 0;
     slideNum: number;
-    inScrollProcess: boolean = false;
     charts: Array<any> = [];
     loadContentAni: Array<boolean> = []; //indicator for content load animation
     easeContentAni: Array<boolean> = []; //indicator for content ease(fade away) animation
     pageLayoutConfig: Array<any> = [];
     inEaseProcess = false;
     @ViewChildren('chart') chartEle: any;
+
+
     constructor(
         private windowResizeService: WindowResizeService,
         private pageScrollService: PageScrollService,
         private slidesService: SlidesService,
-
         @Inject(DOCUMENT) private document: any,
         private sanitizer: DomSanitizer,
         private router: Router,
         private route: ActivatedRoute
-
-
     ) {
         /* config of scroll*/
-        PageScrollConfig.defaultScrollOffset = 50;
-        PageScrollConfig.defaultEasingLogic = {
-            ease: (t: number, b: number, c: number, d: number): number => {
-                // easeInOutExpo easing
-                if (t === 0) return b;
-                if (t === d) return b + c;
-                if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-                return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
-            }
-        };
+        // PageScrollConfig.defaultScrollOffset = 50;
+        PageScrollConfig.defaultInterruptible = false;
+        PageScrollConfig.defaultDuration = 800;
+        // PageScrollConfig.defaultEasingLogic = {
+        //     ease: (t: number, b: number, c: number, d: number): number => {
+        //         // easeInOutExpo easing
+        //         if (t === 0) return b;
+        //         if (t === d) return b + c;
+        //         if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+        //         return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        //     }
+        // };
         /*set the slide size to fit window size*/
         this.windowResizeService.height$.subscribe(height => {
             this.slideHeight_style = {
@@ -65,13 +64,13 @@ export class SlidesPresentationComponent implements OnInit, AfterViewInit, After
 
     }
     ngOnInit() {
+
         let id;
         this.route.params.subscribe(params => {
             id = params['id'];
         });
         /* generate and initialize slides*/
-        this.slidesService.getSlides(id)
-            .subscribe(
+        this.slidesService.getSlides(id).subscribe(
             slide => {
                 this.slides = slide.slides;
                 this.slideNum = this.slides.length;
@@ -137,15 +136,8 @@ export class SlidesPresentationComponent implements OnInit, AfterViewInit, After
           window.scrollTo(0,0);//scroll to top everytime open the slides
 
     }
-    ngAfterViewInit() {
-
-
-    }
-    ngAfterViewChecked() {
-
-    }
     /*init the charts*/
-    initCharts() {
+    private initCharts() {
         let charts = this.chartEle.toArray();
         let chartCounter = 0;
         //if there is no graph on the i-th slide, then store  chart[i] as null
@@ -184,7 +176,9 @@ export class SlidesPresentationComponent implements OnInit, AfterViewInit, After
     loadContent(index) {
         if (!this.pageLayoutConfig[index].hasText) return false;
         this.loadContentAni[index] = false;
-        setTimeout(_ => { this.easeContentAni[index] = false; this.loadContentAni[index] = true }, 625);
+        // setTimeout(_ => { 
+            this.easeContentAni[index] = false; this.loadContentAni[index] = true 
+        // }, 625);
     }
     easeContent(index) {
         //    if (this.inEaseProcess) return;
@@ -195,8 +189,10 @@ export class SlidesPresentationComponent implements OnInit, AfterViewInit, After
         setTimeout(_ => { this.loadContentAni[index] = false; this.easeContentAni[index] = true }, 0);
         setTimeout(_ => this.inEaseProcess = false, 50);
     }
-    /*slide switch operation*/
-    lastSlide() {
+    // slide switch operation
+
+
+    private lastSlide() {
         /*  if (this.charts.length == 0 || this.charts === undefined) {
               this.initCharts();
           }*/
@@ -215,11 +211,11 @@ export class SlidesPresentationComponent implements OnInit, AfterViewInit, After
 
         }
     }
-    nextSlide() {
+
+    private nextSlide() {
         /*  if (this.charts.length == 0 || this.charts === undefined) {
               this.initCharts();
           }*/
-
 
         this.curSlideIndex = this.getCurSlideIndex();
         if (this.curSlideIndex < this.slideNum) {
@@ -240,24 +236,18 @@ export class SlidesPresentationComponent implements OnInit, AfterViewInit, After
 
         }
     }
-    releaseLock() {
 
-
+    private goToSlide(index: number) {
+      let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, '#slide-' + index);
+      this.pageScrollService.start(pageScrollInstance);
     }
-    goToSlide(index: number): void {
-        setTimeout(_ => {
-            let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, '#slide-' + index);
-            this.pageScrollService.start(pageScrollInstance);
 
-
-        }, 0)
-
-    };
-    getCurSlideIndex(): number {
+    private getCurSlideIndex(): number {
         let scrollDis = document.body.scrollTop;
         let curIndex = Math.round(scrollDis / this.slideHeight);
         return curIndex;
     }
+
     check() {
         return true;
     }
