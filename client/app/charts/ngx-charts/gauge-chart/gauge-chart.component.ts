1887b8a0de164392896b8519d8df3fd0ebe2bde2
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-05-26 11:23:17 +0200

    clean code

--- client/app/charts/ngx-charts/gauge-chart/gauge-chart.component.ts
+++ client/app/charts/ngx-charts/gauge-chart/gauge-chart.component.ts
@@ -37,35 +37,6 @@ export class GaugeChartComponent implements OnInit, Chart {
 
   tooltipDisabled = false;
 
-
-
-  single = [
-    {
-      name: 'Germany',
-      value: 40632
-    },
-    {
-      name: 'USA',
-      value: 49737
-    },
-    {
-      name: 'France',
-      value: 36745
-    },
-    {
-      name: 'United Kingdom',
-      value: 36240
-    },
-    {
-      name: 'Spain',
-      value: 33000
-    },
-    {
-      name: 'Italy',
-      value: 35800
-    }
-  ];
-
   constructor() { }
 
   ngOnInit() {
