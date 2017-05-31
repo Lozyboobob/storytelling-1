3bb96f42f351ef9827d4048aa017722b92bca5d6
ghersi.cherifa <ghersi.cherifa@gmail.com>
2017-05-31 15:57:53 +0200

    add the HierarchicalEdgeBundlingComponent in index.ts

--- client/app/charts/index.ts
+++ client/app/charts/index.ts
@@ -4,4 +4,5 @@ export * from './force-directed-graph/force-directed-graph.component';
 export * from './line-chart/line-chart.component';
 export * from './treemap-chart/treemap-chart.component';
 export * from './sunburst-chart/sunburst-chart.component';
+export * from './hierarchical-edge-bundling/hierarchical-edge-bundling.component';
 export * from './ngx-charts';
