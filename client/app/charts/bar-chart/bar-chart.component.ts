b07de39dd2a8ba4787756040566fd779d93ac9e3
liu xinyu <137150808@qq.com>
2017-05-31 14:39:49 +0200

    add interativity to bar chart

--- client/app/charts/bar-chart/bar-chart.component.ts
+++ client/app/charts/bar-chart/bar-chart.component.ts
@@ -10,7 +10,7 @@ export class BarChartComponent implements OnInit, Chart {
 
     @ViewChild('chart') private chartContainer: ElementRef;
     private data: Array<any> = sample;
-    private margin: any = { top: 20, bottom: 20, left: 20, right: 20 };
+    private margin: any = { top: 0, bottom: 0, left: 40, right: 40 };
     private chart: any;
     private width: number;
     private height: number;
@@ -30,6 +30,7 @@ export class BarChartComponent implements OnInit, Chart {
     createChart() {
         let element = this.chartContainer.nativeElement;
         this.width = element.offsetWidth - this.margin.left - this.margin.right;
+        console.log(this.width, element.offsetWidth);
         this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
 
         let svg = d3.select(element).append('svg')
@@ -42,9 +43,7 @@ export class BarChartComponent implements OnInit, Chart {
             .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
 
         // define X & Y domains
-        console.log("index-1",this.data);
         let xDomain = this.data.map(d => d.index);
-        console.log("index-1");
         let yDomain = [0, d3.max(this.data, d => d.value)];
 
         // create scales
@@ -52,7 +51,7 @@ export class BarChartComponent implements OnInit, Chart {
         this.yScale = d3.scaleLinear().domain(yDomain).range([this.height, 0]);
 
         // bar colors
-        this.colors = d3.scaleLinear().domain([0, this.data.length]).range(<any[]>['red', 'blue']);
+        this.colors = d3.scaleOrdinal(d3.schemeCategory20);
 
         // x & y axis
         this.xAxis = svg.append('g')
@@ -63,25 +62,20 @@ export class BarChartComponent implements OnInit, Chart {
             .attr('class', 'axis axis-y')
             .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
             .call(d3.axisLeft(this.yScale));
-    }
 
-    updateChart() {
+
+
         // update scales & axis
         this.xScale.domain(this.data.map(d => d.index));
         this.yScale.domain([0, d3.max(this.data, d => d.value)]);
-        this.colors.domain([0, this.data.length]);
+        //this.colors.domain([0, this.data.length]);
         this.xAxis.transition().call(d3.axisBottom(this.xScale));
         this.yAxis.transition().call(d3.axisLeft(this.yScale));
 
-        let update = this.chart.selectAll('.bar')
-            .data(this.data);
 
-        // remove exiting bars
-        update.exit().remove();
+        let bars = this.chart.selectAll('.bar')
 
-        // update existing bars
-        this.chart.selectAll('.bar').transition()
-
+        bars.transition()
             .attr('x', d => this.xScale(d.index))
             .attr('y', d => this.yScale(d.value))
             .attr('width', d => this.xScale.bandwidth())
@@ -89,15 +83,74 @@ export class BarChartComponent implements OnInit, Chart {
             .style('fill', (d, i) => this.colors(i));
 
         // add new bars
-        update
+        /*
+                bars
+                    .enter()
+                    .append('rect')
+                    .attr('class', 'bar')
+                    .attr('x', d => this.xScale(d.index))
+                    .attr('y', d => this.yScale(0))
+                    .attr('width', this.xScale.bandwidth())
+                    .attr('height', 0)
+                    .attr('opacity', 0.8)
+                    .style('fill', (d, i) => this.colors(i))
+                    .on('mouseover', _ => {
+                        d3.select(d3.event.srcElement).attr('opacity', 1)
+                        //this.chart.selectAll('.value-text').attr('opacity', 1)
+                    })
+                    .on('mouseout', _ => {
+                        d3.select(d3.event.srcElement).attr('opacity', 0.8)
+                        //this.chart.selectAll('.value-text').attr('opacity', 1)
+                    });
+
+                //value tip at the top
+                bars
+                    .enter()
+                    .append('text')
+                    .attr('class', 'value-text')
+                    .attr('font-weight', 600)
+                    .attr('x', d => this.xScale(d.index) + this.xScale.bandwidth() / 2)
+                    .attr('y', d => this.yScale(d.value) - 5)
+                    .attr("text-anchor", "middle")
+                    .attr('fill', (d, i) => this.colors(i))
+                    .attr('opacity', 1)
+                    .text(d => d.value);
+        */
+        bars.data(this.data)
             .enter()
+            .append("g")
+            .attr('class', 'bar-block')
+            .on('mouseover', _ => {
+              console.log(d3.event)
+                d3.select(d3.event.srcElement).attr('opacity', 1)
+                d3.select(d3.event.target.nextElementSibling).attr('font-size', '24px')
+            })
+            .on('mouseout', _ => {
+                d3.select(d3.event.srcElement).attr('opacity', 0.8)
+                d3.select(d3.event.target.nextElementSibling).attr('font-size', '14px')
+            });
+
+        this.chart.selectAll('.bar-block')
             .append('rect')
             .attr('class', 'bar')
             .attr('x', d => this.xScale(d.index))
             .attr('y', d => this.yScale(0))
             .attr('width', this.xScale.bandwidth())
             .attr('height', 0)
+            .attr('opacity', 0.8)
             .style('fill', (d, i) => this.colors(i))
+
+
+        this.chart.selectAll('.bar-block')
+            .append('text')
+            .attr('class', 'value-text')
+            .attr('font-weight', 600)
+            .attr('x', d => this.xScale(d.index) + this.xScale.bandwidth() / 2)
+            .attr('y', d => this.yScale(d.value) - 5)
+            .attr("text-anchor", "middle")
+            .attr('fill', (d, i) => this.colors(i))
+            .attr('opacity', 1)
+            .text(d => d.value);
 
         this.chart.selectAll('.bar').transition()
             //.delay((d, i) => i * 100 + 400)
@@ -105,44 +158,48 @@ export class BarChartComponent implements OnInit, Chart {
             .attr('height', d => this.height - this.yScale(d.value));
 
     }
+
+    updateChart() {
+
+
+    }
     setData(data) {
         if (data.length == 0) return;
-        console.log("data",data);
+        console.log("data", data);
         this.data = data;
     }
     init() {
-        console.log("start init")
         this.createChart();
-        if (this.data.length) {
-            this.updateChart();
-        }
-        console.log("finish init");
     }
     load() {
         this.loaded = true;
         this.chart.selectAll('.bar')
-            .attr('x', d => this.xScale(d.index))
             .attr('y', d => this.yScale(0))
-            .attr('width', d => this.xScale.bandwidth())
             .attr('height', d => this.height - this.yScale(0))
-            .style('fill', (d, i) => this.colors(i));
+
+        console.log("load");
+        this.chart.selectAll('.value-text')
+            .attr('opacity', 0)
 
         this.chart.selectAll('.bar').transition()
             .delay((d, i) => i * 100 + 400)
             .attr('y', d => this.yScale(d.value))
             .attr('height', d => this.height - this.yScale(d.value));
+        this.chart.selectAll('.value-text').transition()
+            .delay((d, i) => i * 100 + 400)
+            .duration(200)
+            .attr('opacity', 1);
 
     }
     ease() {
         this.loaded = false;
         this.chart.selectAll('.bar').transition()
             .delay((d, i) => i * 100)
-            .attr('x', d => this.xScale(d.index))
             .attr('y', d => this.yScale(0))
-            .attr('width', d => this.xScale.bandwidth())
             .attr('height', d => this.height - this.yScale(0))
-            .style('fill', (d, i) => this.colors(i));
-
+        this.chart.selectAll('.value-text').transition()
+            .duration(200)
+            .attr('opacity', 0);
         let waitingTime = this.chart.selectAll('.bar')._groups[0].length * 100;
         /* if the chart doesn't load after certain time ( the time allows chart ease and then load again*/
         setTimeout(_ => {
@@ -151,6 +208,11 @@ export class BarChartComponent implements OnInit, Chart {
                 .delay((d, i) => i * 100)
                 .attr('y', d => this.yScale(d.value))
                 .attr('height', d => this.height - this.yScale(d.value));
+            this.chart.selectAll('.value-text').transition()
+                .delay((d, i) => i * 100)
+                .duration(200)
+                .attr('opacity', 1);
+
         }, waitingTime * 2 + 400);
     }
 
