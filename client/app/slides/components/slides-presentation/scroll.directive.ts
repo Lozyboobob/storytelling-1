c16b7ea518c9a723acac8343c23044a69ef790b6
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-06-07 10:40:29 +0200

    change scroll strategy

--- client/app/slides/components/slides-presentation/scroll.directive.ts
+++ client/app/slides/components/slides-presentation/scroll.directive.ts
@@ -6,23 +6,25 @@ import { Observable } from "rxjs";
 export class ScrollDirective implements OnInit {
     scrollPose: number = 1250; // scroll in same direction won't be triggered repeatedly inside <number> second
     private enableScroll: boolean = true;
-    private delayDuration: number = 100;
+    private delayDuration: number = 25;
     @Output() mouseWheelUp = new EventEmitter();
     @Output() mouseWheelDown = new EventEmitter();
+    @Output() mouseScroll = new EventEmitter();
 
     constructor() {
     }
 
     ngOnInit(){
-        let events = Observable.fromEvent(window, 'mousewheel', (e) => e);
+        let events = Observable.fromEvent(window, 'scroll', (e) => e);
         let firstEventObservable = events.take(1);
         let remainingEventsObservable = events.skip(1).timeInterval()
                 .filter(x => x.interval >= this.delayDuration)
                 .map(x => x.value);
         let pageChangeObservable = firstEventObservable.concat(remainingEventsObservable);
 
-        pageChangeObservable.subscribe(e => this.mouseWheelFunc(e));
+        pageChangeObservable.subscribe(e => this.mouseScroll.emit());
     }
+    
     // @HostListener('mousewheel', ['$event'])
     // private onMouseWheelChrome(event: any) {
     //     console.log('event => ', event);
@@ -34,30 +36,33 @@ export class ScrollDirective implements OnInit {
     
     // }
 
-    @HostListener('DOMMouseScroll', ['$event'])
-    private onMouseWheelFirefox(event: any) {
-        this.mouseWheelFunc(event);
-    }
+    // @HostListener('DOMMouseScroll', ['$event'])
+    // private onMouseWheelFirefox(event: any) {
+    //     this.mouseWheelFunc(event);
+    // }
 
-    @HostListener('onmousewheel', ['$event'])
-    private onMouseWheelIE(event: any) {
-        this.mouseWheelFunc(event);
-    }
+    // @HostListener('onmousewheel', ['$event'])
+    // private onMouseWheelIE(event: any) {
+    //     this.mouseWheelFunc(event);
+    // }
 
-    private mouseWheelFunc(event: any) {
-        //  if (!this.enableScroll) return;
-        var event = window.event || event; // old IE support
-        // for IE
-        event.returnValue = false;
-        // for Chrome and Firefox
-        if (event.preventDefault) {
-            event.preventDefault();
-        }
-        var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
-        if (delta > 0) {
-            this.mouseWheelUp.emit();
-        } else if (delta < 0) {
-            this.mouseWheelDown.emit();
-        }
-    }
+    // private mouseWheelFunc(event: any) {
+    //     console.log(event);
+    //     //  if (!this.enableScroll) return;
+    //     var event = window.event || event; // old IE support
+    //     // for IE
+    //     event.returnValue = false;
+    //     // for Chrome and Firefox
+    //     if (event.preventDefault) {
+    //         event.preventDefault();
+    //     }
+    //     var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
+    //     if (delta > 0) {
+    //         this.mouseWheelUp.emit();
+    //     } else if (delta < 0) {
+    //         this.mouseWheelDown.emit();
+    //     }else{
+    //         this.mouseScroll.emit();
+    //     }
+    // }
 }
