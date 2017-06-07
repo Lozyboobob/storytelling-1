a782566708c682d7872de2cdb6bebdf8d849c90a
ghersi.cherifa <ghersi.cherifa@gmail.com>
2017-06-07 13:13:49 +0200

    fix bug

--- client/app/slides/components/slides-manager/slides-manager.component.ts
+++ client/app/slides/components/slides-manager/slides-manager.component.ts
@@ -12,7 +12,12 @@ export class SlidesManagerComponent implements OnInit {
     @select(['session', 'token']) loggedIn$: Observable<string>;
     private states = ['All', 'Private', 'Public'];
     private selectedValue = 'All';
-    Result = {noResult : false, noPublish : false, noSlides: false, noPrivate : false};
+    result = {
+        noResult : false,
+        noPublish : false,
+        noSlides: false,
+        noPrivate : false
+    };
     toSearch = {
         title: '',
         filter: 'All'
@@ -23,13 +28,13 @@ export class SlidesManagerComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-      this.slidesService.getSlidesList()
+        this.slidesService.getSlidesList()
           .subscribe(
           slide => {
               console.log('toto: ', slide);
               /*slide.forEach(s => this.slides.push(new SlidesListItem(s.slidesSetting)))*/
               this.slides = slide;
-              this.Result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title )
+              this.result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title );
           },
           error => {
               console.log('fail to get Slides list');
@@ -44,7 +49,7 @@ export class SlidesManagerComponent implements OnInit {
             .subscribe(slides => {
                 this.slides = [];
                 this.slides = slides;
-                this.Result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title )
+                this.result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title )
             });
     }
 
@@ -54,7 +59,7 @@ export class SlidesManagerComponent implements OnInit {
             .subscribe(slides => {
                 this.slides = [];
                 this.slides = slides;
-                this.Result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title )
+                this.result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title )
 
             });
     }
@@ -63,27 +68,26 @@ export class SlidesManagerComponent implements OnInit {
             .subscribe(slides => {
                 this.slides = [];
                 this.slides = slides;
-               this.Result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title )
+                this.result = this.calculResult(this.slides.length, this.toSearch.filter, this.toSearch.title );
 
             });
     }
 
-    calculResult(slidesLength, state, title){
-        let result;
+    calculResult(slidesLength, state, title) {
         if (slidesLength === 0) {
-            if (title === '') {
-                if (state === 'All') {
-                    result = {noResult : false, noPublish : false, noSlides: true, noPrivate : false};
-                } else if (state === 'Public') {
-                    result = {noResult : false, noPublish : true, noSlides: false, noPrivate : false};
-                } else if (state === 'Private') {
-                    result = {noResult : false, noPublish : false, noSlides: false, noPrivate : true};
+            if (title === "") {
+                if (state === "All") {
+                    return {noResult : false, noPublish : false, noSlides: true, noPrivate : false};
+                } else if (state ==="Public") {
+                    return {noResult : false, noPublish : true, noSlides: false, noPrivate : false};
+                } else if (state === "Private") {
+                    return {noResult : false, noPublish : false, noSlides: false, noPrivate : true};
                 }
             } else {
-                result = {noResult : true, noPublish : false, noSlides: false, noPrivate : false};
+                return {noResult : true, noPublish : false, noSlides: false, noPrivate : false};
             }
         }
-        return result;
+        return {noResult : false, noPublish : false, noSlides: false, noPrivate : false};
     }
 
 }
