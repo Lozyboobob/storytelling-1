9d9c6d24b0d80d8dd5dbac0d0df364ec4a7710e7
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-06-08 17:11:45 +0200

    fix scroll

--- client/app/slides/components/slides-presentation/scroll.directive.ts
+++ client/app/slides/components/slides-presentation/scroll.directive.ts
@@ -6,7 +6,7 @@ import { Observable } from "rxjs";
 export class ScrollDirective implements OnInit {
     scrollPose: number = 1250; // scroll in same direction won't be triggered repeatedly inside <number> second
     private enableScroll: boolean = true;
-    private delayDuration: number = 25;
+    private delayDuration: number = 100;
     @Output() mouseWheelUp = new EventEmitter();
     @Output() mouseWheelDown = new EventEmitter();
     @Output() mouseScroll = new EventEmitter();
@@ -15,20 +15,22 @@ export class ScrollDirective implements OnInit {
     }
 
     ngOnInit() {
-        let events = Observable.fromEvent(window, 'scroll', (e) => e);
+        let events = Observable.fromEvent(window, 'mousewheel', (e) => e);
         let firstEventObservable = events.take(1);
         let remainingEventsObservable = events.skip(1).timeInterval()
             .filter(x => x.interval >= this.delayDuration)
             .map(x => x.value);
         let pageChangeObservable = firstEventObservable.concat(remainingEventsObservable);
 
-        pageChangeObservable.subscribe(e => this.mouseScroll.emit());
+        pageChangeObservable.subscribe(e => this.mouseWheelFunc(e));
     }
-    @HostListener('mousewheel', ['$event']) private onMouseWheelChrome(event: any) {
-        console.log('event => ', event); Observable.of(event).do(console.log).debounceTime(30).map(this.mouseWheelFunc); this.mouseWheelFunc(event);
-    }
-    @HostListener('DOMMouseScroll', ['$event']) private onMouseWheelFirefox(event: any) { this.mouseWheelFunc(event); }
-    @HostListener('onmousewheel', ['$event']) private onMouseWheelIE(event: any) { this.mouseWheelFunc(event); } private mouseWheelFunc(event: any) {
+    // @HostListener('mousewheel', ['$event']) private onMouseWheelChrome(event: any) {
+    //     console.log('event => ', event); Observable.of(event).do(console.log).debounceTime(30).map(this.mouseWheelFunc); this.mouseWheelFunc(event);
+    // }
+    // @HostListener('DOMMouseScroll', ['$event']) private onMouseWheelFirefox(event: any) { this.mouseWheelFunc(event); }
+    // @HostListener('onmousewheel', ['$event']) private onMouseWheelIE(event: any) { this.mouseWheelFunc(event); } 
+    
+    private mouseWheelFunc(event: any) {
         console.log(event);     //  if (!this.enableScroll) return;     var event = window.event || event; // old IE support
         // for IE
         event.returnValue = false;
