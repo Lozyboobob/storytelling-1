e8e9ab46ed0dfc24c2901569fea06f59d2bccee3
liu xinyu <137150808@qq.com>
2017-06-13 17:29:26 +0200

    fix bug slides undefined

--- client/app/slides/models/slides.ts
+++ client/app/slides/models/slides.ts
@@ -4,10 +4,13 @@ export class Slides {
     _id: string;
     slidesSetting: SlidesSetting;
     slides: Array<Slide> = [];
-    constructor(slides ?:Slides ) {
-      //for copy slides
-        this.slidesSetting=slides.slidesSetting;
-        this.slidesSetting.title=this.slidesSetting.title+" "+"copy"
-        this.slides=slides.slides;
+    constructor(slides?: Slides) {
+        //for copy slides
+        if (slides) {
+        this.slidesSetting = slides.slidesSetting;
+            this.slidesSetting.title = this.slidesSetting.title + " " + "copy"
+            this.slides = slides.slides;
+        }
+
     }
 }
