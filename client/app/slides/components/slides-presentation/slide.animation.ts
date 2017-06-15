1373ed673aa0fa59d328c08dea4dde0c50fee168
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-06-15 23:51:50 +0200

    fix animation with aot

--- client/app/slides/components/slides-presentation/slide.animation.ts
+++ client/app/slides/components/slides-presentation/slide.animation.ts
@@ -1,23 +1,19 @@
 import {trigger, state, animate, style, transition} from '@angular/animations';
 
 export function slideTransition() {
-  return slideSwitch();
-}
-
-
-function slideSwitch() {
   return trigger('routerTransition', [
     state('void', style({position:'fixed', width:'100%'}) ),
     state('*', style({position:'fixed', width:'100%'}) ),
 
     transition('* => 1', [
       style({transform: 'translateX(100%)'}),
-      animate('1.0s ease-in-out', style({transform: 'translateX(0%)'}))
+      animate('0.5s ease-in-out', style({transform: 'translateX(0%)'}))
     ]),
     transition('* => -1', [
       style({transform: 'translateX(-100%)'}),
-      animate('1.0s ease-in-out', style({transform: 'translateX(0%)'}))
+      animate('0.5s ease-in-out', style({transform: 'translateX(0%)'}))
     ])
   ]);
 }
+
 
