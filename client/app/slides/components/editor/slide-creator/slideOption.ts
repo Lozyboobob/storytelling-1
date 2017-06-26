2962f653d0dc6bd6ebd209819647e0c0dfc2857b
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-06-26 19:01:24 +0200

    fix Page layout

--- client/app/slides/components/editor/slide-creator/slideOption.ts
+++ client/app/slides/components/editor/slide-creator/slideOption.ts
@@ -35,25 +35,25 @@ export const pageLayoutOption: Array<any> = [
     {
         value: "FullScreenGraph",
         type: "Full Screen Graph",
-        icon:"fa-area-chart"
+        icon: ["fa-area-chart"]
     }, {
         value: "textInCenter",
         type: "Text in Center",
-        icon:"fa-align-center"
+        icon: ["fa-align-center"]
     },
     {
         value: "textInCenterImageBackground",
         type: "Text + Image Background",
-        icon:"fa-picture-o"
+        icon: ["fa-picture-o"]
     },
     {
         value: "LeftGraphRightText",
         type: "Graph on Left +  Text on Right",
-        icon:"fa-align-right"
+        icon: ["fa-align-right", "fa-area-chart"]
     },
     {
         value: "LeftTextRightGraph",
         type: "Text on Left +  Graph on Right",
-        icon:"fa-align-left"
+        icon: ["fa-area-chart", "fa-align-left"]
     }
 ];
