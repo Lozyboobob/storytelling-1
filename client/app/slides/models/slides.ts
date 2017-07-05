b68d6d202ad4c2d9ffb52381b4f61769ca1b720b
liu xinyu <137150808@qq.com>
2017-07-05 11:25:49 +0200

    fix #367 Copy slides can't copy banner pic

--- client/app/slides/models/slides.ts
+++ client/app/slides/models/slides.ts
@@ -7,7 +7,9 @@ export class Slides {
     constructor(slides?: Slides) {
         //for copy slides
         if (slides) {
-        this.slidesSetting = slides.slidesSetting;
+            this.slidesSetting = slides.slidesSetting;
+            //transfer banner object to banner id
+            if (this.slidesSetting.banner._id) this.slidesSetting.banner = this.slidesSetting.banner._id;
             this.slidesSetting.title = this.slidesSetting.title + " " + "copy"
             this.slides = slides.slides;
         }
