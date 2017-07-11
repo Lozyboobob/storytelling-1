056138c41b732eb15903a525380fd68a2e335d0f
liu xinyu <137150808@qq.com>
2017-07-11 17:23:27 +0200

    :art: clean right-graph-left-text-slide

--- client/app/slides/components/slides-presentation/slide-types/right-graph-left-text-slide/right-graph-left-text-slide.component.ts
+++ client/app/slides/components/slides-presentation/slide-types/right-graph-left-text-slide/right-graph-left-text-slide.component.ts
@@ -1,5 +1,5 @@
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { Component, OnInit, AfterContentInit, OnChanges, SimpleChanges, AfterViewInit, Input, ViewChild, ViewChildren, ComponentFactoryResolver, ViewContainerRef, ComponentRef, ChangeDetectionStrategy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit, AfterContentInit, OnChanges, SimpleChanges, Input, ViewChild, ViewChildren, ComponentFactoryResolver, ViewContainerRef, ComponentRef, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from "rxjs/Observable";
 import { Slide } from "../../../../models";
 import { PageConfig, HALF_HALF_LAYOUT } from "../../pageConfig";
@@ -16,9 +16,6 @@ import { ChartsService } from "../../../../services";
 export class RightGraphLeftTextSlideComponent implements OnInit, AfterContentInit, OnChanges  {
 
   @Input() slide: Slide;
-  @Input() pos: number;
-  @Input() slideload$: Observable<number>;
-  @Input() slideease$: Observable<number>;
 
   @ViewChild('parent', { read: ViewContainerRef })
   parent: ViewContainerRef;
@@ -28,13 +25,10 @@ export class RightGraphLeftTextSlideComponent implements OnInit, AfterContentIni
   loadContentAni: boolean = false;
   easeContentAni: boolean = false;
 
-  constructor(private _componentFactoryResolver: ComponentFactoryResolver,
+  constructor(
+    private _componentFactoryResolver: ComponentFactoryResolver,
     private chartsService: ChartsService,
     private sanitizer: DomSanitizer) { }
-
-  ngAfterViewInit() {
-
-  }
 
   ngOnInit() {
     this.setConfig();
@@ -86,25 +80,7 @@ export class RightGraphLeftTextSlideComponent implements OnInit, AfterContentIni
         }
 
     }
-/*
-    ngOnChanges(changes: SimpleChanges) {
-        if (this.slide.graph === 'noGraph' || this.slide.graph === 'image') return;
-        let cmpType: string = this.slide.graph.charAt(0).toUpperCase() + this.slide.graph.slice(1) + 'Component';
-        this.setChart(cmpType);
-
-    }
-
-    private setChart(chartType: string) {
-        const componentFactory = this._componentFactoryResolver.resolveComponentFactory(this.chartsService.getChartType(chartType));
-        this.parent.clear();
-        if (this.componentRef) {
-            this.componentRef.destroy();
-        }
-        this.componentRef = this.parent.createComponent(componentFactory);
-        this.componentRef.instance.dataInput = this.slide.data; // set the input inputData of the abstract class Chart
-        this.componentRef.instance.configInput = this.slide.config; // set the input inputData of the abstract class Chart
-    }
-*/
+    
     private setConfig() {
         this.config = new PageConfig();
         Object.assign(this.config, HALF_HALF_LAYOUT);
@@ -116,38 +92,5 @@ export class RightGraphLeftTextSlideComponent implements OnInit, AfterContentIni
             this.config.hasChart = true;
         };
     }
-
-    // private loadChart() {
-    //   if (this.config.hasChart) {
-    //     (<Chart>this.componentRef.instance).load();
-    //   }
-    // }
-
-    // private easeChart() {
-    //   if (this.config.hasChart) {
-    //     (<Chart>this.componentRef.instance).ease();
-    //   }
-    // }
-
-
-    // private loadContent() {
-    //   if (this.config.hasText) {
-    //     this.loadContentAni = false;
-    //     setTimeout(_ => {
-    //       this.easeContentAni = false;
-    //       this.loadContentAni = true;
-    //     }, 150);
-    //   }
-    // }
-
-    // private easeContent() {
-    //   if (this.config.hasText) {
-    //     this.easeContentAni = false;
-    //     setTimeout(() => {
-    //       this.loadContentAni = false;
-    //       this.easeContentAni = true;
-    //     }, 0);
-    //   }
-    // }
 
 }
